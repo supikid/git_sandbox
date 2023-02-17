89a0fb9917ac42ef8643272cb2b003a6291b97b8
shigyamamoto <supi@SupisAir2022.local>
2023-02-17 17:07:08 +0900

    SampleLib.php作成

---
+++ app/Libs/SampleLib.php
@@ -0,0 +1,22 @@
+<?php
+
+use Carbon\Carbon;
+
+class SampleLib{
+    
+    /**
+     * 本日の日付(Y年m月d日)を取得する
+     */
+    public function getTodayDate(){
+        $dt = Carbon::today();
+        return $dt->format('Y年m月d日');
+    }
+
+    /**
+     * 曜日を取得する（0:日曜 1:月曜 ... 6:土曜）
+     */
+    public function getDayOfWeek($dt){
+        return $dt->dayOfWeek;
+    }
+    
+}
