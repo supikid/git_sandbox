59ac5168bc1e58ebcb3e8607b71dd0ba3ba21fc1
shigyamamoto <supi@SupisAir2022.local>
2023-02-17 20:45:20 +0900

    もとにもどす

--- app/Libs/SampleLib.php
+++ app/Libs/SampleLib.php
@@ -4,7 +4,6 @@ use Carbon\Carbon;
 
 class SampleLib
 {
-
     /**
      * 本日の日付(Y年m月d日)を取得する
      */
@@ -12,49 +11,6 @@ class SampleLib
     {
         $dt = Carbon::today();
         return $dt->format('Y年m月d日');
-    }
-
-    /**
-     * 本日の日付(Ymd)を取得する
-     */
-    public function getTodayDateNum()
-    {
-        $dt = Carbon::today();
-        return $dt->format('Ymd');
-    }
-
-    /**
-     * 曜日を取得する
-     */
-    public function getDayOfWeekStr($dt)
-    {
-        $num = $this->getDayOfWeek($dt);
-        $ret = "";
-        switch ($num) {
-            case 0:
-                $ret = "日";
-                break;
-            case 1:
-                $ret = "月";
-                break;
-            case 2:
-                $ret = "火";
-                break;
-            case 3:
-                $ret = "水";
-                break;
-            case 4:
-                $ret = "木";
-                break;
-            case 5:
-                $ret = "金";
-                break;
-            case 6:
-                $ret = "土";
-                break;
-        }
-        $ret .= "曜日";
-        return $ret;
     }
 
     /**
